fa1011cb0f1c8ba6456e17669d85f0d49a84ff0b
Kevin Sewell <kevin.w.sewell@gmail.com>
2015-12-14 18:27:50 +0200

    Upgrading Selenium version

--- bin/build-run.sh
+++ bin/build-run.sh
@@ -3,4 +3,6 @@
 BASEDIR=$(dirname $0)
 . ${BASEDIR}/../../scripts/build-run.sh
 
+JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_13.jdk/Contents/Home/
+
 build-run ${BASEDIR} com.kungfudev.cloud.registry.RegistryApplication
